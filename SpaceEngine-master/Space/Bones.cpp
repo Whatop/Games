14322fd904f8cd339db66e3cea73de5393d06f67
Whatop <73518664+Whatop@users.noreply.github.com>
2021-01-31 01:04:44 +0900

    2021-01-31

--- SpaceEngine-master/Space/Bones.cpp
+++ SpaceEngine-master/Space/Bones.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "Bones.h"
 
-Bones::Bones()
+Bones::Bones(Vec2 Pos, std::string type, std::string size, bool)
 {
+	m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone.png");
+	m_Bones->SetParent(this);
+	SetPosition(Pos);
 }
 
 Bones::~Bones()
@@ -11,10 +14,12 @@ Bones::~Bones()
 
 void Bones::Update(float deltaTime, float Time)
 {
+
 }
 
 void Bones::Render()
 {
+	m_Bones->Render();
 }
 
 void Bones::OnCollision(Object* obj)
--- SpaceEngine-master/Space/Bones.h
+++ SpaceEngine-master/Space/Bones.h
@@ -4,9 +4,7 @@ class Bones : public Object
 	Sprite* m_Bones;
 
 public:
-	Bones();
-
-	Bones(std::string type, std::string size, bool );
+	Bones(Vec2 Pos,std::string type, std::string size, bool );
 	~Bones();
 
 	void Update(float deltaTime, float Time);
