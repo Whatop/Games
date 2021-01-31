4a4844fc8c42b841fb85d6619baba027256565f5
Whatop <73518664+Whatop@users.noreply.github.com>
2021-01-31 23:26:27 +0900

    2021-01-31

--- SpaceEngine-master/Space/Bones.cpp
+++ SpaceEngine-master/Space/Bones.cpp
@@ -3,7 +3,27 @@
 
 Bones::Bones(Vec2 Pos, std::string type, std::string size, bool)
 {
-	m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone.png");
+if (type == "basic") {
+	if (size == "small")
+		m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone_small.png");
+
+	if (size == "middle")
+		m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone.png");
+
+	if (size == "big")
+		m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone_big.png");
+
+	if (size == "long")
+		m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone_long.png");
+
+	if (size == "lie")
+		m_Bones = Sprite::Create(L"Painting/sans/Attack/Bone_lie.png");
+}
+if (type == "blue") {
+	m_Bones->R = 0;
+	m_Bones->G = 168;
+	m_Bones->B = 255;
+}
 	m_Bones->SetParent(this);
 	SetPosition(Pos);
 }
